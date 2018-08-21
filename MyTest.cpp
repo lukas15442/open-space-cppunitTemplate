a5e6f6ee56b253ee83830194fd284ed85953e811
Lukas Köhler <lukas.koehler@h-da.de>
2018-08-21 21:07:23 +0200

    cppunit Template

---
+++ MyTest.cpp
@@ -0,0 +1,17 @@
+//
+// Created by lukas koehler on 21.08.18.
+//
+
+#include "MyTest.h"
+
+void MyTest::setUp() {
+    TestFixture::setUp();
+}
+
+void MyTest::tearDown() {
+    TestFixture::tearDown();
+}
+
+void MyTest::myTestFunction() {
+    //TODO: make your testings here, or create new functions
+}
